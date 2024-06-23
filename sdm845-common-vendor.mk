3fa731cf508a1627d1017143d30cb2dd17f613e7
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:44:46 +0100

    sdm845-common: Add IMS libs symlinks
    Change-Id: I32558148ce637542b8663a8d8aedb4fcddfdd092

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -773,3 +773,7 @@ PRODUCT_PACKAGES += \
     com.qti.dpmframework \
     dpmapi \
     qcrilhook
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64

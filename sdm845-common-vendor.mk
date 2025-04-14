951ef0a027d4c5dcc295233b27cc33f6408bc1b1
neroices <git@slce.moe>
2025-04-14 12:32:15 +0900

    sdm845-common: Drop MiDirac

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -240,7 +240,6 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface_proprietary \
     libwfdutils_proprietary \
     libasphere \
-    libdirac \
     libshoebox \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \

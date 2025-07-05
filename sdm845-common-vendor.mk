1d050678a045cac4b28b4c9213d8547d5a96390d
duckyduckg <duckyduckg65@gmail.com>
2025-07-05 16:17:32 +0500

    sdm845-common: bring back Hotwords
    wcd-spi mem allocation is fixed meaning now dsp works so bring them back.
    
    Fixed in:
    https://github.com/duckyduckG/android_kernel_xiaomi_sdm845_419/commit/599fdf018836c403afeabfa25dba0f5101cd20d5
    https://github.com/duckyduckG/android_kernel_xiaomi_sdm845_419/commit/bffaa2ee41414166436b10ea207e06ee40cd33a3
    
    Revert "sdm845-common: remove HotwordEnrollment*"
    
    This reverts commit 002150e09105dd8f1ede4dd4767a49cde612defb.
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -12,6 +12,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/dpm/dpm.conf:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/dpm/dpm.conf \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/init/dpmd.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/dpmd.rc \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/init/wfdservice.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/wfdservice.rc \
+    vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/permissions/com.qti.dpmframework.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.qti.dpmframework.xml \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/permissions/dpmapi.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/dpmapi.xml \
     vendor/xiaomi/sdm845-common/proprietary/system_ext/etc/permissions/qcrilhook.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/qcrilhook.xml \
@@ -516,6 +517,8 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
+    HotwordEnrollmentOKGoogleWCD9340 \
+    HotwordEnrollmentXGoogleWCD9340 \
     PowerOffAlarm \
     QtiTelephonyService \
     WfdService \
@@ -524,6 +527,7 @@ PRODUCT_PACKAGES += \
     WfdCommon \
     audiosphere \
     tcmclient \
+    com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
     qcrilhook \
